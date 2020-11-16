024e284896238f73dd3be6f39a9da94e9752951c
bluebeacon8402 <crsorge@icloud.com>
2020-11-16 18:41:55 -0500

    updated exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = "";
+
+            for (int i = word.Length - 1; i < word.Length; i++)
+            {
+                reversed += word[i];
+            }
+
+            if (reversed ==  word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,32 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using PalindromeExercise;
 
-namespace PalindromeExerciseTests
+
+
+namespace Palindrome.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("madam", true)]
+        [InlineData("anna", true)]
+        [InlineData("mom", true )]
+        [InlineData("noon", true)]
+        [InlineData("hello", false)]
+        public void Test1(string word, bool expected)
         {
+
+            //arrange
+            var tester = new WordSmith();
+
+            //act
+            bool actual = tester.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
+
 
         }
     }
